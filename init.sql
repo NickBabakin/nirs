44975ecb8f15b18fa450c188b231d8124b734a97
NickBabakin <NickBabakin@yandex.ru>
2023-12-08 23:15:47 +0300

    now i need idea

--- init.sql
+++ init.sql
@@ -1,5 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 CREATE TABLE IF NOT EXISTS numbers (
     id serial PRIMARY KEY,
     number BIGINT,
     timestamp BIGINT
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id serial PRIMARY KEY,
+    username VARCHAR(50),
+    password TEXT
 );
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 import psycopg2
 
 from sqlalchemy import create_engine, text
+import pandas as pd
 
 db_name = 'db'
 db_user = 'user'
@@ -10,18 +11,18 @@ db_pass = 'passw'
 db_host = 'localhost'
 db_port = '5432'
 
+
 # Connect to the database
 db_string = 'postgresql://{}:{}@{}:{}/{}'.format(db_user, db_pass, db_host, db_port, db_name)
 db = create_engine(db_string)
 
+key = "top_secret"
 
-def add_new_row(n):
-    # Insert a new number into the 'numbers' table.
-    with db.engine.begin() as conn:
-        conn.execute(text("INSERT INTO numbers (number,timestamp) " + \
-                          "VALUES (" + \
-                          str(100) + "," + \
-                          str(int(round(time.time() * 1000))) + ");"))
+
+def parse_ms(s: str):
+    if s[-2:] != 'ms':
+        print(f"ERR: {s[-2:]} is not ms")
+    return float(s[-8:-3])
 
 
 def get_response_time(result):
@@ -30,61 +31,75 @@ def get_response_time(result):
     for row in result:  # iterating to get last row of answer
         second_to_last = last
         last = row[0]
-    return [second_to_last, last]
+    return parse_ms(second_to_last) + parse_ms(last)
 
-def get_explain_get_last_row():
-    query = "EXPLAIN ANALYZE " + \
-            "SELECT number " + \
-            "FROM numbers " + \
-            "WHERE timestamp >= (SELECT max(timestamp) FROM numbers)" + \
-            "LIMIT 1"
 
+
+
+def explain_insert(username: str):
+    password = f"passw{username}"
+    query = (
+        f"EXPLAIN (ANALYZE, COSTS OFF, TIMING OFF)"
+        f"INSERT INTO users (username, password) "
+        f"VALUES ('{username}', '{password}')"
+    )
     with db.engine.begin() as conn:
         result = conn.execute(text(query))
         return get_response_time(result)
 
 
-def get_explain_insert_row():
-    query = "EXPLAIN ANALYZE " + \
-            "INSERT INTO numbers (number,timestamp) " + \
-                          "VALUES (" + \
-                          str(100) + "," + \
-                          str(int(round(time.time() * 1000))) + ");"
-
+def explain_e_insert(username: str):
+    password = f"passw{username}"
+    query = (
+        f"EXPLAIN (ANALYZE, COSTS OFF, TIMING OFF)"
+        f"INSERT INTO users (username, password) "
+        f"VALUES ('{username}', pgp_sym_encrypt('{password}', '{key}', 'compress-algo=1, cipher-algo=aes256'))"
+    )
     with db.engine.begin() as conn:
         result = conn.execute(text(query))
         return get_response_time(result)
 
-def get_explain_insert_e_row():
-    query = "EXPLAIN ANALYZE " + \
-            "INSERT INTO numbers (number,timestamp) " + \
-                          "VALUES (" + \
-                          str(100) + "," + \
-                          str(int(round(time.time() * 1000))) + ");"
 
+def explain_e_select():
+    query = (
+        f"EXPLAIN (ANALYZE, COSTS OFF, TIMING OFF)"
+        f"SELECT username, pgp_sym_decrypt(password::bytea, '{key}') as password "
+        f"FROM users where username = 'oleg_e';"
+    )
     with db.engine.begin() as conn:
         result = conn.execute(text(query))
         return get_response_time(result)
 
+
+def e_select():
+    query = (
+        f"SELECT username, pgp_sym_decrypt(password::bytea, '{key}') as password "
+        f"FROM users where username = 'oleg_e';"
+    )
+    with db.engine.begin() as conn:
+        result = conn.execute(text(query))
+        for row in result:
+            print(row)
+
+
+def delete_from_users():
+    query = (
+        f"DELETE FROM users; "
+    )
+    with db.engine.begin() as conn:
+        conn.execute(text(query))
+        return
 
 
 if __name__ == '__main__':
-    print('Usual insert')
-    times = []
-    for i in range(10):
-        cur_time = get_explain_insert_row()
-        print(i, cur_time)
-        times.append(cur_time)
+    delete_from_users()
+    ids = range(1000)
+    list_usual = [[explain_insert(str(i))] for i in ids]
+    list_enc = [[explain_e_insert(str(i))] for i in ids]
+    df_usual = pd.DataFrame(list_usual, columns=['time_ms'])
+    df_enc = pd.DataFrame(list_enc, columns=['time_ms'])
 
-    time.sleep(3)
-
-    print('Encrypted insert')
-    e_times = []
-    for i in range(10):
-        cur_time = get_explain_insert_e_row()
-        print(i, cur_time)
-        e_times.append(cur_time)
-
-    #print("total times:\n", times)
+    print(df_usual.mean())
+    print(df_enc.mean())
 
 
